0bac0b4e7fbf4dc9934ecce54079ba6aca8b2d67
jayhao6 <15608018591@163.com>
2025-10-25 17:42:59 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -1,9 +1,13 @@
-//
-//
-//
+//202510422132
+//15608018591@163.com
+//蒋周皓
 #include <stdio.h>
-int main()
-{
-    printf("Hello World");
+
+int main() {
+    float celsius, fahrenheit;
+    printf("请输入摄氏温度: ");
+    scanf("%f", &celsius);
+    fahrenheit = celsius * 9 / 5 + 32;
+    printf("华氏温度: %.f\n", fahrenheit);
     return 0;
 }
